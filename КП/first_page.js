87d90ef854b9a77190d8bc17e6a9edab2c73fb65
Slnpa <96545526+Slnpa@users.noreply.github.com>
2022-05-10 01:44:17 +0300

    Add files via upload

---
+++ КП/first_page.js
@@ -0,0 +1,57 @@
+function initialize() {
+    /* First_buttom */
+    $(document).ready(function() {
+        $(".buttom").click(() => {
+            $(".buttom").hide(500);
+            $(".buttom_return").show(500);
+            $(".second_container_add").show(300);
+        });
+    });
+    $(document).ready(function() {
+        $(".buttom_return").click(() => {
+            $(".buttom_return").hide();
+            $(".buttom").show(500);
+            $(".second_container_add").hide(300);
+        });
+    });
+    /* Second_buttom */
+    $(document).ready(function() {
+        $(".buttom_read").click(() => {
+            $(".buttom_read").hide(500);
+            $(".buttom_read_return").show(500);
+            $(".third_container_add").show(300);
+        });
+    });
+    $(document).ready(function() {
+        $(".buttom_read_return").click(() => {
+            $(".buttom_read_return").hide(500);
+            $(".buttom_read").show(500);
+            $(".third_container_add").hide(300);
+        });
+    });
+
+    $(document).ready(function() {
+        $("#future").click(() => {
+            alert("Никаких:)")
+        });
+    });
+
+    $(document).ready(function() {
+        $("#des").click(() => {
+            $(".tex").show();
+            $(".act").hide();
+            $(".video").hide();
+        });
+        $("#actors").click(() => {
+            $(".tex").hide();
+            $(".act").show();
+            $(".video").hide();
+        });
+        $("#trailer").click(() => {
+            $(".tex").hide();
+            $(".act").hide();
+            $(".video").show();
+        });
+    });
+}
+$(initialize);
